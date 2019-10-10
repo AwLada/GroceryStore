4ec5ff0ff7a914bfad75ddfd1aedd1be9872703b
awlada <awlada@mail.csuchico.edu>
2019-10-09 20:44:30 -0700

    Lab x16

--- main.cpp
+++ main.cpp
@@ -10,14 +10,15 @@ int main()
         char input;
         string item;
 
-
+do
+{
         cout<<"\n==GROCERY LIST MANAGER==";
         cout<<"\nEnter your choice: ";
         cout<<"\n (A)dd an item";
         cout<<"\n (Q)uit";
         cout<<"\nYour choice (A/Q): ";
         cin>>input;
-        while (toupper (input) != 'Q'){
+      //  while (toupper (input) != 'Q'){
                 if (toupper (input) == 'A' && numItems < 5)
                 {
                         cout<<"What is the item?"<<endl;
@@ -33,7 +34,7 @@ int main()
 
                 }
 
-                if (numItems != 5)
+              /*  if (numItems != 5)
                 {
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
@@ -41,7 +42,17 @@ int main()
                 cout<<"\n (Q)uit";
                 cout<<"\nYour choice (A/Q): ";
                 cin>>input;
-                }
+                } */
         }
+while ( toupper (input) != 'Q');
+if ( toupper (input) == 'Q' )
+{
+cout<<"=ITEMS TO BUY=="<<endl;
+cout<<"1 "<<list[0]<<endl;
+cout<<"2 "<<list[1]<<endl;
+cout<<"3 "<<list[2]<<endl;
+cout<<"4 "<<list[3]<<endl;
+cout<<"5 "<<list[4]<<endl;
+}
         return 0;
 }
