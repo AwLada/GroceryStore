0fe21b91f2410c99d28f4ec54933eba4823a61f5
awlada <awlada@mail.csuchico.edu>
2019-10-14 21:57:49 -0700

    vector

---
+++ second.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+        vector<string> list;
+        int numItems = 0;
+        char input;
+        string item;
+
+        do
+        {
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+
+                if (toupper (input) == 'A')
+                {
+                       
+                                cout<<"What is the item?"<<endl;
+                                cin>>item;
+                                
+                                list.push_back (item);
+                                numItems += 1;
+                        
+                }
+        } while ( toupper (input) != 'Q');
+
+        if ( toupper (input) == 'Q' && numItems == 0 )
+        { 
+                cout<< "No items to buy!"<<endl;
+        }
+        else if ( toupper (input) == 'Q')
+        {cout<<"=ITEMS TO BUY=="<<endl;
+                for (int i=0; i<= (list.size() -1); i++)
+
+                {
+                        cout << i << " : " << list[i] << endl;
+                }
+        }
+        return 0;
+}
