2dca641f2f11e38a81aa394b81e253035c47827d
awlada <awlada@mail.csuchico.edu>
2019-10-28 16:59:59 -0700

    Lab 21 changed for turn in

--- second.cpp
+++ second.cpp
@@ -9,6 +9,7 @@ int main()
         vector<string> list;
         int numItems = 0;
         char input;
+        string deleted;
         string item;
 
         do
@@ -18,45 +19,46 @@ int main()
                 cout<<"\n (A)dd an item";
                 if ( list.size () != 0 )
                 {
-                        cout<<"\n (D)elete an item";
+                        cout<<"\n (D)elete last item";
                 }
                 cout<<"\n (Q)uit";
-                if ( list.size () != 0)
-                {
-                        cout<<"\nYour choice (A/D/Q): ";
-                }
-                if ( list.size () == 0 )
-                {
+                /*if ( list.size () != 0)
+                  {
+                  cout<<"\nYour choice (A/D/Q): ";
+                  }*/
+                
                         cout<<"\nYour choice (A/Q): ";
+
+                        cin>>input;
+
+                        if (toupper (input) == 'D' )
+                        {
+                                deleted = list[list.size () -1];
+                                list.pop_back ();
+                                cout<< deleted << "was deleted from the list." << endl;
+                        }
+                        if  (toupper (input) == 'A')
+                        {
+
+                                cout<<"What is the item?"<<endl;
+                                cin>>item;
+
+                                list.push_back (item);
+                                numItems += 1;
+                        }
+                } while ( toupper (input) != 'Q');
+
+                if ( toupper (input) == 'Q' && numItems == 0 )
+                { 
+                        cout<< "No items to buy!"<<endl;
                 }
-                cin>>input;
+                else if ( toupper (input) == 'Q')
+                {cout<<"=ITEMS TO BUY=="<<endl;
+                        for (int i=0; i<= (list.size() -1); i++)
 
-                if (toupper (input) == 'D' )
-                {
-                        list.pop_back ();
+                        {
+                                cout << i << " " << list[i] << endl;
+                        }
                 }
-                if  (toupper (input) == 'A')
-                {
-
-                        cout<<"What is the item?"<<endl;
-                        cin>>item;
-
-                        list.push_back (item);
-                        numItems += 1;
-                }
-        } while ( toupper (input) != 'Q');
-
-        if ( toupper (input) == 'Q' && numItems == 0 )
-        { 
-                cout<< "No items to buy!"<<endl;
+                return 0;
         }
-        else if ( toupper (input) == 'Q')
-        {cout<<"=ITEMS TO BUY=="<<endl;
-                for (int i=0; i<= (list.size() -1); i++)
-
-                {
-                        cout << i << " : " << list[i] << endl;
-                }
-        }
-        return 0;
-}
